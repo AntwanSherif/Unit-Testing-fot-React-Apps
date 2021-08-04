75b32337bd02af530efee082f6ef45bc87683c1b
AntwanSherif <antwansherif@gmail.com>
2021-08-05 00:25:23 +0200

    :white_check_mark: Replace rendering logic with render function

--- src/components/counter/__tests__/Counter.spec.js
+++ src/components/counter/__tests__/Counter.spec.js
@@ -1,9 +1,5 @@
-import ReactDOM from 'react-dom';
+import { render } from '@testing-library/react';
 import { Counter } from '../Counter';
-
-beforeEach(() => {
-  document.body.innerHTML = '';
-});
 
 const clickEvent = new MouseEvent('click', {
   bubbles: true,
@@ -11,32 +7,15 @@ const clickEvent = new MouseEvent('click', {
   button: 0
 });
 
-// rendering
-// behavior
 it('Counter increments and decrements when the buttons are clicked', () => {
-  // setup
-  const container = document.createElement('div');
-  document.body.append(container);
-
-  ReactDOM.render(<Counter />, container);
-
-  //   console.log(document.body.innerHTML);
+  const { container } = render(<Counter />);
 
   const message = container.firstChild.querySelector('div');
   const [decrement, increment] = container.querySelectorAll('button');
 
-  // act & expect
-
   expect(message.textContent).toBe('Current count: 0');
-  //   increment.click();
   increment.dispatchEvent(clickEvent);
   expect(message.textContent).toBe('Current count: 1');
-
-  //   decrement.click();
   decrement.dispatchEvent(clickEvent);
-
   expect(message.textContent).toBe('Current count: 0');
-
-  // clean up
-  container.remove();
 });
