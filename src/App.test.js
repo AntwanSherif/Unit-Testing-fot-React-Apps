ab59df90b2e0c86e834cc9e419c3bfec7c3772b8
AntwanSherif <antwansherif@gmail.com>
2021-08-04 19:58:52 +0200

    :fire: Remove unnecessary files

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders learn unit-testing in react', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/learn unit testing in react/i);
   expect(linkElement).toBeInTheDocument();
 });
