b975e89bb14ac4790f3e86e8f3abe4d0c8aa83cb
AntwanSherif <antwansherif@gmail.com>
2021-08-05 14:09:50 +0200

    :recycle: Add custom render method

--- src/components/button/__tests__/Button.spec.js
+++ src/components/button/__tests__/Button.spec.js
@@ -1,13 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen } from 'test/test-utils';
 import { Button } from '../Button';
-import { ThemeProvider } from '../theme';
 
-function renderWithTheme(ui, { theme = 'light' } = {}) {
-  const Wrapper = ({ children }) => <ThemeProvider initialTheme={theme}>{children}</ThemeProvider>;
-  render(ui, { wrapper: Wrapper });
-}
 it('render with the light theme when the light theme is set', () => {
-  renderWithTheme(<Button>Click me</Button>);
+  render(<Button>Click me</Button>);
   const button = screen.getByRole('button', { name: /click me/i });
 
   expect(button).toHaveStyle(`
@@ -17,7 +12,7 @@ it('render with the light theme when the light theme is set', () => {
 });
 
 it('render with the dark theme when the dark theme is set', () => {
-  renderWithTheme(<Button>Click me</Button>, { theme: 'dark' });
+  render(<Button>Click me</Button>, { theme: 'dark' });
   const button = screen.getByRole('button', { name: /click me/i });
 
   expect(button).toHaveStyle(`
---
+++ src/test/test-utils.js
@@ -0,0 +1,12 @@
+import { render as rtlRender } from '@testing-library/react';
+import { ThemeProvider } from 'components';
+
+function render(ui, { theme = 'light', ...options } = {}) {
+  const Wrapper = ({ children }) => <ThemeProvider initialTheme={theme}>{children}</ThemeProvider>;
+  return rtlRender(ui, { wrapper: Wrapper, ...options });
+}
+
+export * from '@testing-library/react';
+
+// override React Testing Library's render with our own
+export { render };
