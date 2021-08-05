f4bb7044eec2a2f582c2afa8798bfb2ed7de11bf
AntwanSherif <antwansherif@gmail.com>
2021-08-05 13:58:07 +0200

    :recycle: Add renderWithTheme fn to avoid duplication

--- src/components/button/__tests__/Button.spec.js
+++ src/components/button/__tests__/Button.spec.js
@@ -2,13 +2,26 @@ import { render, screen } from '@testing-library/react';
 import { Button } from '../Button';
 import { ThemeProvider } from '../theme';
 
-it('render with the light theme when the light theme is set', () => {
-  const Wrapper = ({ children }) => <ThemeProvider initialTheme='light'>{children}</ThemeProvider>;
+function renderWithTheme({ theme = 'light' } = {}) {
+  const Wrapper = ({ children }) => <ThemeProvider initialTheme={theme}>{children}</ThemeProvider>;
   render(<Button>Click me</Button>, { wrapper: Wrapper });
+}
+it('render with the light theme when the light theme is set', () => {
+  renderWithTheme();
   const button = screen.getByRole('button', { name: /click me/i });
 
   expect(button).toHaveStyle(`
       background-color: white;
       color: black;
     `);
+});
+
+it('render with the dark theme when the dark theme is set', () => {
+  renderWithTheme({ theme: 'dark' });
+  const button = screen.getByRole('button', { name: /click me/i });
+
+  expect(button).toHaveStyle(`
+        color: white;
+        background-color: black;
+      `);
 });
